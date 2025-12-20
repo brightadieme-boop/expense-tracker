f84be9b57cdfebb608fd2634f4563cb0e3d5a936
Bright Adieme <bright.adieme@gmail.com>
2025-12-20 15:23:52 +0000

    Allow vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -14,11 +14,12 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://my-moneytracker-phi.vercel.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
