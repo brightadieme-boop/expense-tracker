b4999800b4d30592bf57a9c2c34e0359ba5db97b
Bright Adieme <bright.adieme@gmail.com>
2025-12-22 13:53:24 +0000

    Final fix for buttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+
 import Navbar from "./components/Navbar.jsx";
 import App from "./App.jsx";
 import "./styles/main.css";
