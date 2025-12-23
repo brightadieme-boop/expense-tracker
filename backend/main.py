4ef1e1c4279b7ea389e0e8a91aa66936e0a93be5
Bright Adieme <bright.adieme@gmail.com>
2025-12-23 02:05:37 +0000

    Fix CORS by adding specific domain

--- backend/main.py
+++ backend/main.py
@@ -14,12 +14,13 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://my-moneytracker-phi.vercel.app"
+    "https://my-moneytracker-phi.vercel.app",
+    "https://monitrackr.com"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
